91bb805dc568868a0f0b23be209c82076b230a19
Lex Robinson <lexi@lexi.org.uk>
2013-12-15 15:37:49 +0000

    Top down view

--- js/maze.js
+++ js/maze.js
@@ -1,42 +1,66 @@
 (function($, _){'use strict';
-	function Matrix(){
-		this.matrix = [
-			1,0,0,0,
-			0,1,0,0,
-			0,0,1,0,
-			0,0,0,1,
-		];
-	}
-	_.extend(Matrix.prototype, {
-		translate: function(x, y, z)
-		{
-			this.matrix[3] += x || 0;
-			this.matrix[7] += y || 0;
-			this.matrix[11] += z || 0;
-			// ???
-		},
-		rotate: function(deg, axis)
-		{
-			// ???
-		},
-		css: function()
-		{
-			return "matrix3d(" + this.matrix.join(',') + ")";
-		}
-	})
+	var i = 0;
+	var tilesize = 200;
 	function Wall(){
+		i++;
 		var e = $('<div>',{
 			'class':'wall',
-			'text': "wat"
+			'text': "wat" + i
 		});
 		console.log('e', e);
 		this.$el = e;
 		this.el = e[0];
-		$('#geometry').append(e)
+		$('#geometry').append(e);
+		this._translateX = 0;
+		this._translateY = 0;
+		this._rotate = false;
+		this.rebuildCSS();
 	}
-	new Wall;
-	var a = new Wall;
-	var m = new Matrix;
-	m.translate(0.1,0,0)
-	a.$el.css('transform', m.css());
-})(jQuery, _)
+	_.extend( Wall.prototype, {
+		_stepAdjust: function(steps)
+		{
+			if ( steps > 0 )
+				steps -= 0.5;
+			else if ( steps < 0 )
+				steps += 0.5;
+			return steps;
+		},
+		translateX: function(steps) {
+			this._translateX = this._stepAdjust( steps );
+			this.rebuildCSS();
+		},
+		translateY: function(steps) {
+			this._translateY = this._stepAdjust( steps );
+			this.rebuildCSS();
+		},
+		rotate: function(yes) {
+			this._rotate = yes;
+			this.rebuildCSS();
+		},
+		rebuildCSS: function()
+		{
+			var arr = [];
+			if ( this._translateX )
+				arr.push( 'translateX(' + this._translateX * tilesize + 'px)' );
+			if ( this._translateY )
+				arr.push( 'translateY(' + this._translateY * tilesize + 'px)' );
+			if ( this._rotate )
+				arr.push( 'rotateZ(90deg)' );
+			arr.push('rotateY(90deg)');
+			arr.push('rotateZ(-90deg)');
+			this.$el.css('transform', arr.join(' ') );
+		}
+	} );
+	// new Wall();
+	var a = new Wall();
+	var b = new Wall();
+	// var c = new Wall();
+	var d = new Wall();
+	a.translateX(1);
+	b.translateX(-1);
+	// c.translateY(1);
+	// c.rotate(true);
+	d.translateY(-1);
+	d.rotate(true);
+
+})(jQuery, _);
