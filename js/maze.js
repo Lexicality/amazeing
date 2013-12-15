2d95f8d31e8d2ee9041f791114971af795bc8fa5
Lex Robinson <lexi@lexi.org.uk>
2013-12-15 16:13:49 +0000

    woop - align walls betterly

--- js/maze.js
+++ js/maze.js
@@ -1,7 +1,7 @@
 (function($, _){'use strict';
 	var i = 0;
 	var tilesize = 200;
-	function Wall(){
+	function Wall( x, y, r ){
 		i++;
 		var e = $('<div>',{
 			'class':'wall',
@@ -14,23 +14,35 @@
 		this._translateX = 0;
 		this._translateY = 0;
 		this._rotate = false;
+		if ( x )
+			this.translateX( x );
+		if ( y )
+			this.translateY( y );
+		if ( r )
+			this.rotate( true );
 		this.rebuildCSS();
 	}
 	_.extend( Wall.prototype, {
-		_stepAdjust: function(steps)
+		_stepAdjust: function(steps, isY)
 		{
+			// return steps;
+			var amt = 0;
+			if ( isY )
+				amt += 0.5
+			if (this._rotate)
+				amt += 0.5;
 			if ( steps > 0 )
-				steps -= 0.5;
-			else if ( steps < 0 )
-				steps += 0.5;
+				steps -= amt;
+			else
+				steps += amt;
 			return steps;
 		},
 		translateX: function(steps) {
-			this._translateX = this._stepAdjust( steps );
+			this._translateX = steps;
 			this.rebuildCSS();
 		},
 		translateY: function(steps) {
-			this._translateY = this._stepAdjust( steps );
+			this._translateY = steps;
 			this.rebuildCSS();
 		},
 		rotate: function(yes) {
@@ -41,9 +53,9 @@
 		{
 			var arr = [];
 			if ( this._translateX )
-				arr.push( 'translateX(' + this._translateX * tilesize + 'px)' );
+				arr.push( 'translateX(' + this._stepAdjust( this._translateX ) * tilesize + 'px)' );
 			if ( this._translateY )
-				arr.push( 'translateY(' + this._translateY * tilesize + 'px)' );
+				arr.push( 'translateY(' + this._stepAdjust( this._translateY, true ) * tilesize + 'px)' );
 			if ( this._rotate )
 				arr.push( 'rotateZ(90deg)' );
 			arr.push('rotateY(90deg)');
@@ -51,16 +63,29 @@
 			this.$el.css('transform', arr.join(' ') );
 		}
 	} );
-	// new Wall();
-	var a = new Wall();
-	var b = new Wall();
-	// var c = new Wall();
-	var d = new Wall();
-	a.translateX(1);
-	b.translateX(-1);
-	// c.translateY(1);
-	// c.rotate(true);
-	d.translateY(-1);
-	d.rotate(true);
-
+	function w(x,y,z) {
+		return new Wall(x,y,z);
+	}
+	// w(0,0,0);
+	// w(0,0,1);
+	w(2,0,1);
+	w(1,0,1);
+	w(-1,0,1);
+	w(-2,0,1);
+	w(0,-1,0);
+	w(0,1,0);
+	// w( 1, 0,0);
+	// w( 1,-1,0);
+	// w( 1, 1,0);
+	// w( 2, 0,0);
+	// w( 2, 1,0);
+	// w( 2,-1,0);
+	// w( 2,-2,0);
+	// w( 2,-2,1);
+	// w(-1, 0,0);
+	// w(-2, 0,0);
+	// w( 0, 1,1);
+	// w( 0,-1,1);
+	// w( 1,-1,1);
 })(jQuery, _);
+
